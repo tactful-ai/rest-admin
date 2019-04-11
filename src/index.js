57609240a05318e6e3ebc8965dac5897b1da751d
Fouad <mohammad.hassan@slickblox.com>
2019-04-11 14:41:01 +0200

    converting to a plugin

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import _ from 'lodash'
+
+import './http'
+import config from './config'
+import inflection from 'inflection'
+import form from './form'
+
+import Snotify from 'vue-snotify'
+import 'font-awesome/css/font-awesome.min.css'
+import 'simple-line-icons/css/simple-line-icons.css'
+import './scss/style.scss'
+
+import "vue-snotify/styles/material.css"
+
+import storage from './storage'
+import routes from './routes'
+
+const restAdminPlugin = {
+    install (Vue, _options) {
+        Vue.prototype.$storage = storage
+        Vue.prototype._ = _
+
+        Vue.prototype.$config = config
+        Vue.prototype.$inflection = inflection
+        
+        form(Vue);
+        Vue.use(Snotify)
+    },
+    routes: routes
+};
+
+export default restAdminPlugin;
---
+++ src/routes.js
@@ -0,0 +1,65 @@
+
+import ResourceIndex from "../views/ResourceIndex";
+import ResourceEdit from "../views/ResourceEdit";
+import ResourceShow from "../views/ResourceShow";
+import ResourceStat from "../views/ResourceStat";
+import CustomForm from "../views/CustomForm";
+import CustomPage from "../views/CustomPage";
+import CustomTable from "../views/CustomTable";
+import Home from "../views/Home";
+
+routes: [
+  {
+    path: "/",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/rest/:resource/stat/:type?",
+    name: "stat",
+    component: ResourceStat
+  },
+  {
+    path: "/rest/:resource",
+    name: "index",
+    component: ResourceIndex
+  },
+  {
+    path: "/rest/:resource/create/:group?",
+    name: "create",
+    component: ResourceEdit
+  },
+  {
+    path: "/rest/:resource/:id/edit/:group?",
+    name: "edit",
+    component: ResourceEdit
+  },
+
+  {
+    path: "/rest/:resource/:id/:group?",
+    name: "show",
+    component: ResourceShow
+  },
+
+  {
+    path: "/form/:uri?",
+    name: "form",
+    component: CustomForm
+  },
+  {
+    path: "/table/:uri?",
+    name: "table",
+    component: CustomTable
+  },
+  {
+    path: "/page/:uri?",
+    name: "page",
+    component: CustomPage
+  }
+];
+
+export default routes;
